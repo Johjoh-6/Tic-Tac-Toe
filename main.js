8cdf57c1395ac3bb96ac6370aa9a404738c0dfef
Johann six <six.johann@gmail.com>
2021-12-24 10:53:59 +0100

    add if empty for add player.symbol

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const btnReset = document.querySelector('#reset');
 const selectSymbol = document.querySelector('#symbol');
 const gameBoard = document.querySelector('#gameboard');
-
+const pTurn = document.querySelector('#turn');
+let currentPlayer;
 
 class Player {
     constructor(
@@ -17,6 +18,10 @@ const player1 = new Player();
 const player2 = new Player();
 // maybe do it a another way to set the player like input at the beggining before showing the board game
 selectSymbol.addEventListener('change', (e)=> {
+    //    Set Game 
+    clearGame();
+    setBoard();
+   
     player1.symbol = this.symbol.value;
     player1.player = 'player ' + this.symbol.value;
     if (symbol.value === 'X'){
@@ -27,50 +32,71 @@ selectSymbol.addEventListener('change', (e)=> {
         player2.player = 'player X';
         player2.symbol = 'X';
     }
-    console.log(player1);
-    console.log(player2);
+    printPlayerTurn();
 })
 
 const grid = [0,1,2,3,4,5,6,7,8];
-const winCombos = [
-            [0,1,2],
-            [0,3,6],
-            [3,4,5],
-            [6,7,8],
-            [1,4,7],
-            [2,4,6],
-            [2,5,8],
-            [0,4,8]
-        ];
-//Set the current
+
+//Set the current player
+currentPlayer = player1;
         function currentP(){
             currentPlayer = currentPlayer  == player1 ? player2 : player1;
+        }
+        function printPlayerTurn(){
+            pTurn.textContent = 'Turn to ' + currentPlayer.player;
         }
 
     function setBoard() {
         for (let i = 0; i < grid.length; i++) {
             const divBoard = document.createElement("div");
-            const pBoard = document.createElement("p");
                 divBoard.setAttribute('data-game', grid.indexOf(i))
-                divBoard.appendChild(pBoard);
                 gameBoard.insertAdjacentElement("beforeend", divBoard);
                 divBoard.addEventListener('click', selected);
+            
         }
+        printPlayerTurn();
     }
     
     function selected(x){
-        const p = document.querySelector('#gameboard p ');
-        console.log(player1.symbol);
-        x.target.textContent = player1.symbol;
+        // if empty add symbol of the player
+        if (x.target.textContent == '' ) {
+            x.target.textContent = currentPlayer.symbol;
+            // grid = currentPlayer.symbol;
+            currentP();
+            printPlayerTurn();
+            console.log(grid[x.target]);
+            console.log(grid.indexOf(x));
+        
+        }
     }
-
+    function clearGame() {
+        gameBoard.querySelectorAll('div').forEach(elem => elem.remove());
+        pTurn.innerHTML = "";
+        }
     // Reset game
    btnReset.addEventListener('click', (e)=> {
-    gameBoard.textContent= '';
-    setBoard();
+    clearGame();
+    selectSymbol.selectedIndex = null;
    }) 
 
 
-//    Set Game 
-setBoard();
-console.log(player1);
+// Check winner 
+const winCombos = [
+    [0,1,2],
+    [0,3,6],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [0,4,8]
+];
+winCombos.forEach(combo => console.log(combo));
+winCombos.forEach(combo => 
+    combo.forEach(function(nb) {
+        console.log(nb)
+        if (nb === 'X') {
+          console.log('win');
+        }
+      }));
+
